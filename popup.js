d74de4974f132b0c7a151a01f8c04ac5ee9bba7f
aleksandrayakovleva132 <33125810+aleksandrayakovleva132@users.noreply.github.com>
2017-12-08 18:55:08 +0300

    промежуточный итог. попап

---
+++ popup.js
@@ -0,0 +1,39 @@
+var link = document.querySelector(".button-feedback");
+var bgpopup = document.querySelector(".feedback-container");
+var popup = document.querySelector(".feedback-popup");
+var close = document.querySelector(".feedback-button-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var comment = popup.querySelector("[name=comment]");
+
+link.addEventListener("click", function(evt){
+   evt.preventDefault(); 
+   popup.classList.add("feedback-popup-show");
+   bgpopup.classList.add("feedback-container-show");
+   login.focus();
+});
+
+close.addEventListener("click", function(evt){
+    evt.preventDefault();
+    popup.classList.remove("feedback-popup-show");
+    bgpopup.classList.remove("feedback-container-show")
+});
+
+bgpopup.addEventListener("click", function(evt){
+    evt.preventDefault();
+    popup.classList.remove("feedback-popup-show");
+    bgpopup.classList.remove("feedback-container-show")
+});
+
+form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    console.log(login.value);
+    console.log(email.value);
+    console.log(comment.value);
+    
+    if (!login.value || !email.value){
+       evt.preventDefault();
+       console.log("Нужно ввести имя и почту"); 
+    };
+});
